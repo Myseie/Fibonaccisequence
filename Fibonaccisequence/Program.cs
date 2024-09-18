6ccb3e338794cc418b279aae342f295a42cf4a09
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-18 08:51:17 +0200

    Add files via upload

---
+++ Fibonaccisequence/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.ComponentModel.Design;
+
+class Program 
+{ 
+static void Main () { 
+Console.Write("Ange vilket nummer i Fibonacciserien du vill ha: ");
+int n = int.Parse(Console.ReadLine());
+
+    int resultat = Fibonacci(n);
+    Console.WriteLine($"Fibonacci {n} är {resultat}");
+
+}
+
+static int Fibonacci(int n)
+{
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
+    {
+        return 1;
+
+    }
+    else
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+}
